a089f1020a6e64f03e5283ca8919af52c1ecef67
Nouman Malik <mrmaliknn@gmail.com>
2020-09-03 18:11:27 +0500

    plz check this

---
+++ operators.php
@@ -0,0 +1,49 @@
+<?php
+echo "<h2>This is the addition</h2>";
+$x = 10;  
+$y = 6;
+
+echo ($x + $y." is the sum of two variables"."<br>");
+
+
+echo "<h2>This is the subtraction</h2>";
+
+$x = 10;  
+$y = 6;
+
+ echo ($x - $y." is the subtraction of two variables"."<br>");
+
+ echo "<h2>This is the multiplication</h2>";
+
+
+ $x = 10;  
+$y = 6;
+
+ echo ($x * $y." is the multiplication of two variables"."<br>");
+
+ echo "<h2>This is the division</h2>";
+
+ $x = 10;  
+$y = 6;
+
+ echo ($x / $y." is the division of two variables"."<br>");
+
+ echo "<h2>This is the modulation</h2>";
+
+
+ $x = 10;  
+$y = 6;
+
+ echo ($x % $y." is the modulus of two variables"."<br>");
+
+ echo "<h2>This is the exponentiation</h2>";
+
+
+ $x = 10;  
+$y = 6;
+
+ echo ($x ** $y." is the exponentiation of two variables"."<br>");
+
+
+
+?>
--- variables.php
+++ variables.php
@@ -1,1 +1,18 @@
-//this is a file
+<?php
+
+//// This is a single-line comment
+/* this is multipule 
+line cmment */
+$x ="Im a string";
+
+echo "$x <br>"; 
+$x = 500;
+echo "$x is a integer <br>";
+$x = 5.444;
+echo "$x is a float or double <br>";
+$x ="A Boolean represents two possible states: TRUE or FALSE. <br>";
+echo "$x";
+$x = array("Volvo","BMW","Toyota");
+var_dump($x);
+
+?>
