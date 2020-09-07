c416ebca89e16e358600cbfb5cfaa65be63cd3d9
Nouman Malik <mrmaliknn@gmail.com>
2020-09-07 11:34:41 +0500

    check plz

--- operators.php
+++ operators.php
@@ -1,4 +1,12 @@
 <?php
+
+// ..............PHP Arithmetic Operators ........... 
+
+
+
+
+
+
 // This is the addition
 $x = 10;  
 $y = 6;
@@ -45,5 +53,82 @@ $y = 6;
  echo ($x ** $y." is the exponentiation of two variables"."<br>");
 
 
+
+
+
+
+<!-- .................PHP Comparison Operators -->
+
+
+
+// PHP COMPARISON OPERATORS
+
+
+// Equal to operator
+
+
+$x = "200";
+$y = 200;
+var_dump($x==$y);
+
+// Identical operator
+
+// in identical operator both value of variable and data type should be equal
+
+$x = 200;
+$y = 200;
+var_dump($x===$y);
+
+
+// not equal operator
+
+$x = 100;
+$y = 200;
+var_dump($x!=$y);
+
+// not equal operator
+
+$x = 100;
+$y = 200;
+var_dump($x<>$y);
+// not identical operator
+
+$x = 100;
+$y = 200;
+var_dump($x!==$y);
+
+
+// greater than operator
+
+$x = 300;
+$y = 200;
+var_dump($x>$y);
+
+
+// less than operator
+
+
+$x = 300;
+$y = 400;
+var_dump($x<$y);
+
+// greater than equal operator
+
+$x = 300;
+$y = 200;
+var_dump($x>=$y);
+
+// less than equal operator
+
+
+$x = 400;
+$y = 400;
+var_dump($x<=$y);
+
+// spaceship operator
+
+$x = 10;
+$y = 20;
+echo ($x<=>$y);
 
 ?>
